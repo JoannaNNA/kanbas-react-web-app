02c6c6a6f31a6f0d280c6231885405ad30afd65f
nazhou <zhou.na@northeastern.edu>
2024-09-19 03:16:53 -0700

    #4

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,12 +1,18 @@
 export default function AssignmentEditor() {
     return (
       <div id="wd-assignments-editor">
-        <label htmlFor="wd-name">Assignment Name</label>
-        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+        <div>
+            <label htmlFor="wd-name">Assignment Name</label>
+            
+        </div><br/>
+        <div>
+            <input id="wd-name" type="text" value="A1 - ENV + HTML" />
+            
+        </div><br/>
         <textarea id="wd-description">
           The assignment is available online Submit a link to the landing page of
-        </textarea>
-        <br />
+        </textarea><br/><br/>
+        
         <table>
           <tr>
             <td align="right" valign="top">
@@ -17,7 +23,87 @@ export default function AssignmentEditor() {
             </td>
           </tr>
           {/* Complete on your own */}
+        </table><br/>
+
+        <div>
+            <label htmlFor="wd-group" className="form-control">Assignment Group</label>
+            <select id="wd-group" className="form-control">
+            <option value="assignment">ASSIGNMENTS</option>
+            </select>
+        </div><br/>
+
+        <div>
+            <label htmlFor="wd-display-grade-as" className="form-control">Display Grade as</label>
+            <select id="wd-display-grade-as" className="form-control">
+            <option value="percentage">Percentage</option>
+            </select>
+        </div><br/>
+
+        <div>
+            <label htmlFor="wd-submission-type" className="form-control">Submission Type</label>
+            <select id="wd-submission-type" className="form-control">
+            <option value="online">Online</option>
+            </select>
+        </div><br/>
+
+            <div>
+            <label>Online Entry Options</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-text-entry"/>
+            <label htmlFor="wd-text-entry">Text Entry</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-website-url"/>
+            <label htmlFor="wd-website-url">Website URL</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-media-recordings"/>
+            <label htmlFor="wd-media-recordings">Media Recording</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-student-annotation"/>
+            <label htmlFor="wd-student-annotation">Student Annotation</label><br/>
+            
+            <input type="checkbox" name="check-genre" id="wd-file-upload"/>
+            <label htmlFor="wd-file-upload">File Update</label>
+            </div>
+
+            <table><br/>
+        <tr>
+            <td align="right" valign="top">
+            <label htmlFor="wd-assign-to">Assign Assign to</label><br/>
+            <input id="wd-assign-to" type="text" value="Everyone" />
+            </td>
+        </tr>
         </table>
+
+        <label htmlFor="wd-due-date"> Due </label>
+        <input type="date"
+            id="wd-due-date"
+            value="2024-05-13"/><br/>
+
+        <label htmlFor="wd-available-from"> Available from </label>
+        <input type="date"
+            id="wd-available-from"
+            value="2024-05-06"/>
+
+        <label htmlFor="wd-available-until"> Until </label>
+        <input type="date"
+            id="wd-available-until"
+            value="2024-05-20"/><br/>
+
+        <hr/>  {/* This is the horizontal rule for a visual separation */}
+
+        <table width="100%"> 
+            <tr>
+                <td align="right">
+                    <button>Cancel</button>
+                    <button>Save</button>
+                </td>
+            </tr>
+        </table>
+
+
+
+    
+
       </div>
   );}
   
--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -2,9 +2,29 @@ export default function CourseStatus() {
     return (
       <div id="wd-course-status">
         <h2>Course Status</h2>
+        <div>
         <button>Unpublish</button> <button>Publish</button>
-        {/* Complete on your own */}
+        </div>
+        <div style={{ height: '20px' }} /> {/* This div acts as a spacer */}
+        <div>
+        <button>Import Existing Content</button>
+        </div>
+         <div>
+        <button>Import from Commons</button>
+        </div>
+         <div>
+        <button>View Course Stream</button> 
+        </div>
+         <div>
+        <button>New Announcement</button> 
+        </div>
+         <div>
+        <button>New Analytics</button> 
+        </div>
+         <div>
         <button>View Course Notifications</button>
+        </div>
+    
       </div>
   );}
   
