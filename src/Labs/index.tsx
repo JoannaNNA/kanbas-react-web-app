abd233550f80a94fa69e1d7d5cd1e46ca9d9079d
nazhou <zhou.na@northeastern.edu>
2024-09-19 10:43:52 -0700

    #Name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import Lab1 from "./Lab1";
-import { Route, Routes, Navigate } from "react-router";
+import { Route, Routes, Navigate } from 'react-router-dom';
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>Labs</h1>
+      <h1>A1</h1>
+      <p>Developed by: Na Zhou</p> 
+      
       <TOC />
+      <a href="https://github.com/JoannaNNA/kanbas-react-web-app.git" id="wd-github">GitHub Repository</a>
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
