56fdcdeb2be7b87758d8a4ec7cef8a9215756da0
nazhou <zhou.na@northeastern.edu>
2024-09-18 21:44:50 -0700

    #2.9

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -7,39 +7,45 @@ export default function Lab1() {
       <h3>HTML Examples</h3>
       <div id="wd-h-tag"> ... </div>
       <div id="wd-p-tag"> ... </div>
-      <div id="wd-lists">
-        <h4>List Tags</h4>
-        <h5>Ordered List Tag</h5>
-        How to make pancakes:
-        <ol id="wd-pancakes">
-          <li>Mix dry ingredients.</li>
-          <li>Add wet ingredients.</li>
-          <li>Stir to combine.</li>
-          <li>Heat a skillet or griddle.</li>
-          <li>Pour batter onto the skillet.</li>
-          <li>Cook until bubbly on top.</li>
-          <li>Flip and cook the other side.</li>
-          <li>Serve and enjoy!</li>
-        </ol>
-        My favorite recipe:
-        <ol id="wd-my-favorite-recipe">{/* complete on your own */}</ol>
-        <h6>Unordered List Tag</h6>
-        My favorite books (in no particular order)
-        <ul id="wd-my-books">
-          <li>Dune</li>
-          <li>Lord of the Rings</li>
-          <li>Ender's Game</li>
-          <li>Red Mars</li>
-          <li>The Forever War</li>
-        </ul>
-        Your favorite books (in no particular order)
-        <ul id="wd-your-books">
-          {/* complete on your own */}
-        </ul>
-
-
+      <div id="wd-lists"> ... </div>
+      <div id="wd-tables">
+        <h4>Table Tag</h4>
+        <table border={1} width="100%">
+          <thead>
+            <tr>
+              <th>Quiz</th>
+              <th>Topic</th>
+              <th>Date</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Q1</td>
+              <td>HTML</td>
+              <td>2/3/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q2</td>
+              <td>CSS</td>
+              <td>2/10/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q3</td>
+              <td>JavaScript</td>
+              <td>2/17/21</td>
+              <td>95</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>Average</td>
+              <td>90</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
-
     </div>
-  );
-}
+);}
