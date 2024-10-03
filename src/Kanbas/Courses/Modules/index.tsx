e3033a408f8143be16aba5c866b879a7a81e4720
nazhou <zhou.na@northeastern.edu>
2024-10-02 18:40:56 -0700

    update

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,17 +1,29 @@
-import ModulesControls from "./ModulesContrals";
+import { BsGripVertical } from "react-icons/bs";
+import ModuleControlButtons from "./ModulesControlButtons";
+import ModulesControls from "./ModulesControls";
+import LessonControlButtons from "./LessonControlButtons";
 
 export default function Modules() {
     return (
       <div>
-        <ModulesControls /><br /><br /><br /><br />
-        <ul id="wd-modules" className="list-group rounded-0">
-          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-            <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
-            <ul className="wd-lessons list-group rounded-0">
-              <li className="wd-lesson list-group-item p-3 ps-1">
-                LEARNING OBJECTIVES </li>
-              <li className="wd-lesson list-group-item p-3 ps-1">
-                Introduction to the course </li>
+          <ModulesControls /><br /><br /><br />
+          <ul id="wd-modules" className="list-group rounded-0">
+            <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+              <div className="wd-title p-3 ps-2 bg-secondary">
+                <BsGripVertical className="me-2 fs-3" />
+                Week 1
+                <ModuleControlButtons />
+              </div>
+              <ul className="wd-lessons list-group rounded-0">
+                <li className="wd-lesson list-group-item p-3 ps-1">
+                  <BsGripVertical className="me-2 fs-3" />
+                  LEARNING OBJECTIVES
+                  <LessonControlButtons />
+                </li>
+                <li className="wd-lesson list-group-item p-3 ps-1">
+                  <BsGripVertical className="me-2 fs-3" />
+                  Introduction to the course
+                  <LessonControlButtons /></li>
               <li className="wd-lesson list-group-item p-3 ps-1">
                 Learn what is Web Development </li>
               <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
