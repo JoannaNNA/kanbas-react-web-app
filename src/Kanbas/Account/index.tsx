fd5e30cee069945d5143cd84cd2a1ed2298be574
nazhou <zhou.na@northeastern.edu>
2024-10-04 20:50:52 -0700

    1

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -2,7 +2,7 @@ import {Routes, Route, Navigate } from "react-router";
 import AccountNavigation from "./Navigation";
 import Signin from "./Signin";
 import Profile from "./Profile";
-import Signup from "./Signup";
+import Signup from "./Signout";
 
 export default function Account() {
     return (
