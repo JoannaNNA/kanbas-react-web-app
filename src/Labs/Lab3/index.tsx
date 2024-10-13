dadb9db5d0005d4a3d08c15cd39c0b7bd805fbe2
nazhou <zhou.na@northeastern.edu>
2024-10-12 17:08:46 -0700

    debug

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -35,6 +35,7 @@ import AddPathParameters from "./AddPathParameters";
 
 
 export default function Lab3() {
+  console.log('Hello World!');
   return (
     <div id="wd-lab3" >
       <h3>Lab 3</h3>
