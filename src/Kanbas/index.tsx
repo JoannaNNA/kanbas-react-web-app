b4dd851722e401b4affe89927a9853013fde58bb
nazhou <zhou.na@northeastern.edu>
2024-11-22 04:02:42 -0800

    4.1

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -5,6 +5,7 @@ import Courses from "./Courses";
 import KanbasNavigation from "./Navigation";
 import "./styles.css";
 import * as db from "./Database";
+
 import { useState } from "react";
 import ProtectedRoute from "./Account/ProtectedRoute";
 
--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,7 +1,11 @@
 import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
   return (
@@ -17,6 +21,11 @@ export default function Lab5() {
       <PathParameters />
       <QueryParameters />
       <WorkingWithObjects />
+      <WorkingWithArrays />
+     
+      <HttpClient />
+      <WorkingWithObjectsAsynchronously />
+      <WorkingWithArraysAsynchronously />
     </div>
   );
 }
